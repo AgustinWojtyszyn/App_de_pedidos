0aa2056e0015953ec9024561a21c0cf712f2ed61
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-17 11:52:12 -0300

    Delete src/hooks/useHelpCenter.js

--- src/hooks/useHelpCenter.js
+++
@@ -1,6 +0,0 @@
-import { useHelpCenterContext } from '../contexts/helpCenterContext'
-
-export default function useHelpCenter() {
-  const { handleUserMessage, handleError, lastResponse, SUPPORT_PHONE, TUTORIAL_URL, MANUAL_ORDER_TEXT } = useHelpCenterContext()
-  return { handleUserMessage, handleError, lastResponse, SUPPORT_PHONE, TUTORIAL_URL, MANUAL_ORDER_TEXT }
-}
